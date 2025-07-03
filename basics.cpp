1fabeeda9f121a121c29cd93ed4327aa86009d20
ramansaini-victus <ramanpreetsaini.18@gmail.com>
2025-07-03 15:26:00 +0530

    basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,69 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello"<<endl<<"Raman";
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     for(int i=0;i<11;i++){
+//         cout<<x<<"x"<<i<<"="<<x * i<< endl;
+//     }
+//     return 0;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float P=100;
+//     float R=50;
+//     float T=2;
+
+//     float SI=(P*R*T)/100;
+//     cout<<SI<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     if(n%2==0){
+//         cout<<"Even"<<endl;
+//     }
+//     else{
+//         cout<<"Odd"<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int flag;
+//     cin>>n;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             flag=1;
+//         }
+//         else{
+//             flag=0;
+//         }
+//     }
+//     if(flag){
+//         cout<<"Prime"<<endl;
+//     }
+//     else{
+//         cout<<"Not prime"<<endl;
+//     }
+//     return 0;
+// }
+
+
+
