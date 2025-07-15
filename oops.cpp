74d2059a6f89dd678f7dfcfcc2810d197463f2d8
ramansaini-victus <ramanpreetsaini.18@gmail.com>
2025-07-15 12:22:25 +0530

    OOPS

---
+++ oops.cpp
@@ -0,0 +1,82 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter number:"<<endl;
+//     cin>>x;
+//     if(x%2==0){
+//         cout<<"Even";
+//     }
+//     else{
+//         cout<<"Odd";
+//     }
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// class Student{
+// public:    
+//     string name;
+//     int rollno;
+//     float cgpa;
+// };
+// int main(){
+//     Student s1;
+//     s1.name="RAMAN";
+//     s1.rollno=616;
+//     s1.cgpa=8.99;
+
+//     Student s2;
+//     s2.name="REETIK";
+//     s2.rollno=617;
+//     s2.cgpa=8.55;
+
+//     cout<<s1.name<<s1.rollno<<s1.cgpa<<endl;
+//     cout<<s2.name<<s2.rollno<<s2.cgpa<<endl;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// class car{
+// public:
+//     string type;
+//     int price;
+//     string name;
+//     int seats;    
+// };
+// void print(car c){
+//     cout<<c.name<<" "<<c.price<<" "<<c.type<<" "<<c.seats<<" "<<endl;
+// }
+// int main(){
+//     car c1;
+//     c1.name="SCORPIO";
+//     c1.price=1800000;
+//     c1.type="SUV";
+//     c1.seats=8;
+
+//     car c2;
+//     c2.name="CRETA";
+//     c2.price=1200000;
+//     c2.type="SUV";
+//     c2.seats=5;
+
+//     car c3;
+//     c3.name="FORTUNER";
+//     c3.price=3600000;
+//     c3.type="SUV";
+//     c3.seats=8;
+
+//     print(c1);
+//     print(c2);
+//     print(c3);
+
+// }
+
+
+
+
+
+
